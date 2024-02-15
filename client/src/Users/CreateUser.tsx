db898fa453331d7bd5fc6786bf878658fcdc0341
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-15 11:07:01 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/Users/CreateUser.tsx
+++ client/src/Users/CreateUser.tsx
@@ -1,6 +1,12 @@
 const CreateUser = () => {
   return (
-    <div>CreateUser</div>
+    <div className="bg-blue-600 w-full h-screen">
+        <div className="container mx-auto py-4 px-12">
+            <div className="bg-white py-4">
+                Add New User
+            </div>
+        </div>
+    </div>
   )
 }
 
