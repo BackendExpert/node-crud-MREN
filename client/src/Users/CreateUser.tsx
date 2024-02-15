f2b1f383ee55ecaca46fab34295c72629a1b0f19
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-15 11:16:28 +0530

    Updaet
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/Users/CreateUser.tsx
+++ client/src/Users/CreateUser.tsx
@@ -1,4 +1,5 @@
 import IonIcon from "@reacticons/ionicons"
+import { Link } from "react-router-dom"
 
 const CreateUser = () => {
   return (
@@ -6,10 +7,7 @@ const CreateUser = () => {
         <div className="container mx-auto py-10 px-12">
             <div className="bg-white py-4 px-12 rounded">
                 <h1 className="text-xl font-semibold">Add New User</h1>
-                <div className="flex md:w-[10%] mt-4 py-2 md:pl-6 text-white rounded bg-blue-500 duration-500 hover:bg-blue-600">
-                    <p className="pt-[2px]"><IonIcon name="arrow-back"></IonIcon></p>
-                    <p className="pl-2">Back</p>
-                </div>
+            
             </div>
         </div>
     </div>
