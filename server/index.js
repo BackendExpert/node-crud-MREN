0546a4932a7ac3dcbb1022ceb11698890932a347
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-15 11:58:40 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/index.js
+++ server/index.js
@@ -10,7 +10,9 @@ app.use(express.json())
 mongoose.connect("mongodb://127.0.0.1:27017/crud")
 
 app.post("/createUser", (req, res) => {
-    
+    UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
 })
 
 app.listen(3001,  () => {
