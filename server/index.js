48bf4c7725c60709c9586a48cecbfb530c75d7a6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-15 11:56:01 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const UserModel = require("./models/UserModel")
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 mongoose.connect("mongodb://127.0.0.1:27017/crud")
+
+app,
 
 app.listen(3001,  () => {
     console.log("Server Running...!")
