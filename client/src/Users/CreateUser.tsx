06cac35123045dcd6cfcee36993e6da8c88b6547
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-15 11:08:51 +0530

    Updaet
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/Users/CreateUser.tsx
+++ client/src/Users/CreateUser.tsx
@@ -4,6 +4,10 @@ const CreateUser = () => {
         <div className="container mx-auto py-10 px-12">
             <div className="bg-white py-4 px-12 rounded">
                 <h1 className="text-xl font-semibold">Add New User</h1>
+                <div className="flex w-[15%]">
+                    <p className="">Back</p>
+                    <p className=""></p>
+                </div>
             </div>
         </div>
     </div>
