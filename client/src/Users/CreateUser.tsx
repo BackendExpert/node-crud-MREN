109ace64c57ddae5dab0728b826ddf35f4cdb18a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-15 11:17:00 +0530

    Updaet
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/Users/CreateUser.tsx
+++ client/src/Users/CreateUser.tsx
@@ -7,7 +7,12 @@ const CreateUser = () => {
         <div className="container mx-auto py-10 px-12">
             <div className="bg-white py-4 px-12 rounded">
                 <h1 className="text-xl font-semibold">Add New User</h1>
-            
+                <Link to={'/Users'}>
+                    <div className="flex md:w-[10%] mt-4 py-2 md:pl-6 text-white rounded bg-blue-500 duration-500 hover:bg-blue-600">
+                        <p className="pt-[2px]"><IonIcon name="arrow-back"></IonIcon></p>
+                        <p className="pl-2">Back</p>
+                    </div>
+                </Link>
             </div>
         </div>
     </div>
