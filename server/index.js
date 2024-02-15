c045b2b2a901f4aee769d4125f4ed60e88e9b076
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-15 11:49:36 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+mongoose.connect("mongodb://127.0.0.1:27017/crud")
+
 app.listen(3001,  () => {
     console.log("Server Running...!")
 })
