36448214000bf0276807ebfd996d2704a331968b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-15 10:42:12 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/Users/Users.tsx
+++ client/src/Users/Users.tsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom"
 
 const Users = () => {
   return (
     <div className="bg-blue-700 w-full h-screen">
         <div className="container mx-auto px-12 py-10">
             <div className="mx-10 py-12 bg-white rounded px-12">
-                Users
+                <p className="font-semibold text-2xl">Users</p>
+                <hr className="pb-10" />
+
             </div>
         </div>
     </div>
