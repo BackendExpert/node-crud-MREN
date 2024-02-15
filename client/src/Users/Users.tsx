33a5019637f1246730ce5f29c9272179e09ec35e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-15 10:41:23 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/Users/Users.tsx
+++ client/src/Users/Users.tsx
@@ -2,8 +2,10 @@
 const Users = () => {
   return (
     <div className="bg-blue-700 w-full h-screen">
-        <div className="container mx-auto px-12">
-            
+        <div className="container mx-auto px-12 py-10">
+            <div className="mx-10 py-12 bg-white rounded px-12">
+                Users
+            </div>
         </div>
     </div>
   )
