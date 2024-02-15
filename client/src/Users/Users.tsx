640dd81189719678b2065af23728644119b803dc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-15 10:51:29 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/Users/Users.tsx
+++ client/src/Users/Users.tsx
@@ -7,13 +7,13 @@ const Users = () => {
         <div className="container mx-auto px-12 py-10">
             <div className="mx-10 py-12 bg-white rounded px-12">
                 <p className="font-semibold text-2xl">Users</p>
-                <hr className="pb-10" />
                 <Link to={'/Users/create'} >
-                    <div className="flex bg-green-700">
+                    <div className="flex bg-green-600 duration-500 hover:bg-green-700 md:w-[15%] md:pl-8 py-2 rounded text-white">
                         <p className="text-md">Add User</p>
                         <p className="pl-2 py-[2px]"><IonIcon name="add"></IonIcon></p>
                     </div>
                 </Link>
+                <hr className="mt-2 mb-4" />
             </div>
         </div>
     </div>
