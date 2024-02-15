c645214382482e870fb0b3f17dd1fff47fa2bf07
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-15 10:36:57 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Users from "./Users/Users";
 import CreateUser from "./Users/CreateUser";
+import UpdateUser from "./Users/UpdateUser";
 
 export default function App() {
   return (
@@ -8,7 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/Users" element={<Users />} ></Route>
         <Route path="/Users/Create" element={<CreateUser />} ></Route>
-        
+        <Route path="/Users/Update" element={<UpdateUser />} ></Route>
       </Routes>
     </BrowserRouter>
   )
